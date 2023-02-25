eea8b895d746e65dd22b1c5d5950c0c7420206e1
giovannifranchi <giovanni.franchi95@gmail.com>
2023-02-25 14:38:06 +0100

    Feat: form check and error handlers

--- js/main.js
+++ js/main.js
@@ -8,4 +8,26 @@ const loginBtn = document.getElementById('login-btn');
 const resetBtn = document.getElementById('reset-btn');
 const humanDices = document.querySelectorAll('.human');
 const computerDices = document.querySelectorAll('.computer');
+const enrolled = ['giovanni.franchi95@gmail.com', 'pinopaolo@hotmail.com', 'davidbowie77@yahoo.com', 'bobdylan@libero.it', 'mariobalotelli@google.it'];
 let isPresent = false;
+
+// Form check
+loginBtn.addEventListener('click', ()=>{
+    nameError.innerHTML = '';
+    lastNameError.innerHTML = '';
+    emailError.innerHTML = '';
+    if(isFinite(userName.value) || isFinite(userLastName.value) || !email.value.includes('@')){ //uso il metodo Include in questo punto perché altrimenti renderebbe il codice molto più lungo, nella parte di check iscritti mostrerò come evitare di usarlo
+        if(isFinite(userName.value)){
+            nameError.innerHTML = 'Inser a valid Name';
+        }
+        if(isFinite(userLastName.value)){
+            lastNameError.innerHTML = 'Inser a valid Lastname';
+        }
+        if(!email.value.includes('@')){
+            emailError.innerHTML = 'Insert a valid email';
+        }
+    }else {
+        // Enrolled check
+    }
+});
+
